6fe1f77789f9e9d7b594e9d00eb4241d47cfe37c
Tim Fennis <fennis.tim@gmail.com>
2023-05-16 20:12:09 +0200

    Day 9 insane optimizations for 0 gain

--- rust/day9/src/main.rs
+++ rust/day9/src/main.rs
@@ -1,3 +1,4 @@
+use arrayvec::ArrayVec;
 use std::collections::{HashMap, HashSet};
 
 type Graph = HashMap<(String, String), usize>;
@@ -30,16 +31,59 @@ fn build_graph() -> Graph {
     graph
 }
 
-fn search(graph: &Graph) -> (usize, usize) {
-    let mut buffer: Vec<(Vec<&str>, usize)> = Vec::with_capacity(100);
+/// Check if the haystack (which is a base16 stack) contains the provided value
+fn path_contains_node(node: usize, path: usize) -> bool {
+    path & 0xF == node
+        || path >> 4 & 0xF == node
+        || path >> 8 & 0xF == node
+        || path >> 12 & 0xF == node
+        || path >> 16 & 0xF == node
+        || path >> 20 & 0xF == node
+        || path >> 24 & 0xF == node
+        || path >> 28 & 0xF == node
+}
 
+fn index_of(needle: &str, haystack: &Vec<&str>) -> usize {
+    let mut i = 1;
+    for c in haystack {
+        if c == &needle {
+            return i;
+        }
+        i += 1;
+    }
+
+    panic!("empty haystack");
+}
+
+fn search(graph: &Graph) -> (usize, usize) {
     let starting_points = graph
         .iter()
         .flat_map(|((from, to), _)| vec![from, to])
         .collect::<HashSet<_>>();
 
-    for start in &starting_points {
-        buffer.push((vec![start], 0));
+    let starting_points = starting_points
+        .iter()
+        .map(|e| e.as_str())
+        .collect::<Vec<_>>();
+
+    let point_count = starting_points.len();
+
+    assert!(point_count < 16);
+
+    // Create a graph that can be searched quickly
+    let graph = graph
+        .iter()
+        .map(|((from, to), distance)| {
+            (index_of(&from, &starting_points) << 32)
+                | (index_of(&to, &starting_points) << 16)
+                | *distance
+        })
+        .collect::<Vec<usize>>();
+
+    let mut buffer = ArrayVec::<(usize, usize), 32>::new();
+
+    for (idx, _start) in starting_points.iter().enumerate() {
+        buffer.push((idx + 1, 0));
     }
 
     let mut best = usize::MAX;
@@ -48,36 +92,29 @@ fn search(graph: &Graph) -> (usize, usize) {
     while let Some((cur_path, cur_dist)) = buffer.pop() {
         // If the current path is a complete path (it includes all locations at least once we
         // can use its length to figure out if it's the the best or worst
-        if cur_dist < best && cur_path.len() == starting_points.len() {
-            best = cur_dist;
-            continue;
-        }
+        if cur_path >> ((point_count - 1) * 4) > 0 {
+            if cur_dist < best {
+                best = cur_dist;
+                continue;
+            }
 
-        if cur_dist > worst && cur_path.len() == starting_points.len() {
-            worst = cur_dist;
-            continue;
+            if cur_dist > worst {
+                worst = cur_dist;
+                continue;
+            }
         }
-
-        // If the current path is incomplete we figure out where we are now
-        let current_location = cur_path.last().unwrap();
 
         // Based on our current location find all the locations we can move to
-        let destinations = graph
-            .iter()
-            .filter(|((from, _), _)| from == current_location);
-
         // For every potential destination figure out if we've already been there
         // if not add the new path to the search buffer
-        for ((_, new_dest), dst_to_dest) in destinations {
-            if !cur_path.contains(&new_dest.as_str()) {
-                // So far this is the fastest way that I've been able to find to add a single
-                // element to a vector
-                let new_path = cur_path
-                    .iter()
-                    .chain([&new_dest.as_str()])
-                    .cloned()
-                    .collect();
-                buffer.push((new_path, cur_dist + dst_to_dest));
+        for &packed in &graph {
+            let new_dest = (packed >> 16) & 0xFFFF;
+            if (packed >> 32) == (cur_path & 0xF) && !path_contains_node(new_dest, cur_path) {
+                // Determine the new path by multiplying by 16 (or shifting left 4 times)
+                let new_path = (cur_path << 4) + new_dest;
+
+                // Add the new path to the sarch buffer
+                buffer.push((new_path, cur_dist + (packed & 0xFFFF)));
             }
         }
     }
